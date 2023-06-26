0e2f5fe355c76b5789af65c24f8130d3b747f3e9
Schuetz, Wolfgang <wolfgang.schuetz@anton-paar.com>
2023-06-26 14:14:14 +0200

    removed byte_sink and byte_source from serializer

--- WLib/WLib_Serializer/inc/WLib_serializer.hpp
+++ WLib/WLib_Serializer/inc/WLib_serializer.hpp
@@ -2,6 +2,7 @@
 #ifndef WLIB_SERIALIZER_HPP
 #define WLIB_SERIALIZER_HPP
 
+#include <WLib_Byte_Sink_Concept.hpp>
 #include <cstddef>
 #include <cstdint>
 #include <type_traits>
@@ -14,9 +15,6 @@ namespace WLib
     little_endian = native,
     big_endian,
   };
-
-  template <typename T> constexpr bool is_byte_sink_v   = std::is_same_v<void, decltype(std::declval<T>()(std::declval<std::byte const&>()))>;
-  template <typename T> constexpr bool is_byte_source_v = std::is_same_v<std::byte, decltype(std::declval<T>()())>;
 
   namespace internal
   {
@@ -164,92 +162,6 @@ namespace WLib
   {
     return deserializer_traits<T>::deserialize(source, byte_order);
   }
-
-  class byte_buffer_source_t
-  {
-  public:
-    template <std::size_t N>
-    constexpr byte_buffer_source_t(std::byte (&buffer)[N])
-        : m_b(&buffer[0])
-        , m_e(&buffer[N])
-        , m_c(&buffer[0])
-    {
-    }
-
-    constexpr byte_buffer_source_t(std::byte* begin, std::byte const* end)
-        : m_b(begin)
-        , m_e(end)
-        , m_c(begin)
-    {
-    }
-
-    constexpr std::byte operator()()
-    {
-      if (this->m_c < this->m_e)
-      {
-        return *this->m_c++;
-      }
-      return std::byte();
-    }
-
-    [[nodiscard]] constexpr std::byte*       begin() const { return this->m_b; }
-    [[nodiscard]] constexpr std::byte const* cbegin() const { return this->m_b; }
-    [[nodiscard]] constexpr std::byte const* end() const { return this->m_c; }
-    [[nodiscard]] constexpr std::byte const* cend() const { return this->m_c; }
-
-    [[nodiscard]] constexpr std::size_t get_number_of_processed_bytes() const { return this->m_c - this->m_b; }
-    [[nodiscard]] constexpr std::size_t get_number_of_remaining_bytes() const { return this->m_e - this->m_c; }
-    constexpr void                      restart() { this->m_c = this->m_b; }
-
-  private:
-    std::byte*       m_b;
-    std::byte const* m_e;
-    std::byte*       m_c;
-  };
-
-  class byte_buffer_sink_t
-  {
-  public:
-    template <std::size_t N>
-    constexpr byte_buffer_sink_t(std::byte (&buffer)[N])
-        : m_b(&buffer[0])
-        , m_e(&buffer[N])
-        , m_c(&buffer[0])
-    {
-    }
-
-    constexpr byte_buffer_sink_t(std::byte* begin, std::byte const* end)
-        : m_b(begin)
-        , m_e(end)
-        , m_c(begin)
-    {
-    }
-
-    constexpr void operator()(std::byte const& x)
-    {
-      if (this->m_c >= this->m_e)
-        return;
-
-      *this->m_c++ = x;
-    }
-
-    [[nodiscard]] constexpr std::byte*       begin() const { return this->m_b; }
-    [[nodiscard]] constexpr std::byte const* cbegin() const { return this->m_b; }
-    [[nodiscard]] constexpr std::byte const* end() const { return this->m_c; }
-    [[nodiscard]] constexpr std::byte const* cend() const { return this->m_c; }
-
-    [[nodiscard]] constexpr std::size_t get_number_of_used_bytes() const { return this->m_c - this->m_b; }
-    [[nodiscard]] constexpr std::size_t get_number_of_remaining_bytes() const { return this->m_e - this->m_c; }
-    void                                clear() { this->m_c = this->m_b; }
-
-    [[nodiscard]] constexpr byte_buffer_source_t get_byte_source() const { return byte_buffer_source_t{ this->m_b, this->m_c }; }
-
-  private:
-    std::byte*       m_b;
-    std::byte const* m_e;
-    std::byte*       m_c;
-  };
-
 }    // namespace WLib
 
 #endif    // !WLIB_SERIALIZER_HPP
