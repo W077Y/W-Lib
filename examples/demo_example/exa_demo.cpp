03e741a504c953e6f8ad18bce19dfd3999a3e236
Wolfgang Schütz <WSchuetz@live.com>
2024-03-21 09:06:33 +0100

    .

--- examples/demo_example/exa_demo.cpp
+++ examples/demo_example/exa_demo.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
+class pimpl_ex
+{
+public:
+  pimpl_ex();
+  ~pimpl_ex();
+
+  int aaa() const;
+
+private:
+  class impl_t;
+
+  impl_t& m_obj;
+};
+
 int main()
 {
-  std::cout << "This is a demo example!" << std::endl;
+
+  auto aa = pimpl_ex();
+
+  std::cout << "This is a demo example! " << aa.aaa() << std::endl;
   return 0;
 }
+
+class pimpl_ex::impl_t
+{
+public:
+  impl_t() = default;
+
+  int aaa() const { return this->m_a; }
+
+private:
+  int m_a = 1;
+};
+
+pimpl_ex::pimpl_ex()
+    : m_obj(*new pimpl_ex::impl_t())
+{
+}
+pimpl_ex::~pimpl_ex() { delete &this->m_obj; }
+
+int pimpl_ex::aaa() const { return this->m_obj.aaa(); }
